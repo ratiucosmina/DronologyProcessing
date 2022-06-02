b57ce26124c90a9838f1c4729a46b4756e159678
ratiucosmina <ratiucosmina@gmail.com>
2022-06-02 17:27:33 +0200

    Links and fields missing

--- src/main/java/JiraToAzureTranslator.java
+++ src/main/java/JiraToAzureTranslator.java
@@ -1,26 +1,29 @@
 import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.IssueField;
+import com.atlassian.jira.rest.client.api.domain.IssueLink;
+import com.atlassian.jira.rest.client.internal.json.IssueJsonParser;
 import org.azd.enums.WorkItemOperation;
 import org.azd.exceptions.AzDException;
 import org.azd.utils.AzDClientApi;
 import org.azd.workitemtracking.WorkItemTrackingApi;
 import org.azd.workitemtracking.types.WorkItem;
+import org.azd.workitemtracking.types.WorkItemRelations;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class JiraToAzureTranslator {
     AzDClientApi azureApi;
     WorkItemTrackingApi witApi;
-    public Map<String, String> mapping = new HashMap<>();
+    public Map<String, WorkItem> mapping = new HashMap<>();
 
     public JiraToAzureTranslator(String organization, String project, String personalAccessToken) {
         this.azureApi = new AzDClientApi(organization, project, personalAccessToken);
         this.witApi = azureApi.getWorkItemTrackingApi();
     }
 
-    public void translateArtifacts(List<Optional<Issue>> issues){
+    public void translateArtifacts(List<Optional<Issue>> issues) {
         for (Optional<Issue> optionalIssue : issues)
             if (optionalIssue.isPresent()) {
                 try {
@@ -29,21 +32,72 @@ public class JiraToAzureTranslator {
                     e.printStackTrace();
                 }
             }
+        for (Optional<Issue> optionalIssue : issues)
+            if (optionalIssue.isPresent()) {
+                createLinks(optionalIssue.get());
+            }
+
     }
 
     private void createWorkItem(Issue issue) throws AzDException {
         String azureType = getAzureType(issue.getIssueType().getName());
-//        WorkItem item = witApi.createWorkItem(azureType, WorkItemOperation.ADD, issue.getSummary(), issue.getDescription(), issue.getLabels().toArray(new String[0]));
-        mapping.put(issue.getKey(), issue.getIssueType().getName());
+        WorkItem item;
+        String title = issue.getSummary()==null? issue.getKey():issue.getSummary();
+        String description = issue.getDescription()==null? "": issue.getDescription();
+        String[] labels = issue.getLabels()==null? new String[0]: issue.getLabels().toArray(new String[0]);
+        item = witApi.createWorkItem(azureType, WorkItemOperation.ADD, title, description, labels);
+        mapping.put(issue.getKey(), item);
     }
+
+    private void createLinks(Issue issue) {
+        WorkItem source = mapping.get(issue.getKey());
+        if (source == null) {
+            System.out.println(issue);
+            return;
+        }
+        List<WorkItemRelations> links = source.getRelations();
+        if (links == null)
+            links = new ArrayList<>();
+
+        IssueField parentField = issue.getField("parent");
+        if (parentField != null && parentField.getValue() != null) {
+            try {
+                JSONObject value = (JSONObject) parentField.getValue();
+                String parentKey = value.get("key").toString();
+                if (!mapping.containsKey(parentKey)) {
+                    System.out.println(parentKey);
+                    System.out.println(issue.getIssueType().getName() + " no visible parent");
+                } else {
+                    WorkItemRelations rel = new WorkItemRelations();
+                    rel.setRel("Child");
+                    rel.setUrl(mapping.get(parentKey).getUrl());
+                    links.add(rel);
+                    System.out.println("YES " + issue.getIssueType().getName() + " "+ issue.getSummary()+ " has parent");
+                }
+            } catch (JSONException e) {
+                System.out.println("Parent parsing failed");
+            }
+        }
+        for (IssueLink link : issue.getIssueLinks()) {
+            WorkItem target = mapping.getOrDefault(link.getTargetIssueKey(), null);
+            if (target == null)
+                continue;
+            WorkItemRelations relations = new WorkItemRelations();
+            relations.setUrl(target.getUrl());
+            relations.setRel(getAzureRelationType(link.getIssueLinkType().getName()));
+            links.add(relations);
+        }
+    }
+
 
     public String getAzureType(String jiraType) {
         switch (jiraType) {
             case "Design Definition":
-                return "Functional Specification";
+//                return "Functional Specification";
+                return "CR Issue FD";
             case "Sub-task":
                 return "Task";
-            case "Process:":
+            case "Process-Requirement":
                 return "Requirement";
             case "Hazard":
             case "Fault":
@@ -51,7 +105,8 @@ public class JiraToAzureTranslator {
             case "Context":
             case "Environmental Assumption":
             case "Rationale":
-                return "Documentation";
+//                return "Documentation";
+                return "Epic";
             case "Acceptance Test":
             case "Simulation":
                 return "Test Case";
@@ -61,6 +116,17 @@ public class JiraToAzureTranslator {
             default:
                 return jiraType;
 
+        }
+    }
+
+    private String getAzureRelationType(String jiraLinkType) {
+        switch (jiraLinkType) {
+            case "Clones":
+                return "Duplicates";
+            case "Tests":
+                return "Tests";
+            default:
+                return "Related";
         }
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import com.atlassian.jira.rest.client.api.domain.Issue;
 import com.atlassian.jira.rest.client.api.domain.IssueLink;
+import org.azd.enums.WorkItemExpand;
+import org.azd.exceptions.AzDException;
+import org.azd.workitemtracking.types.WorkItem;
+import org.azd.workitemtracking.types.WorkItemRelations;
 import org.codehaus.jettison.json.JSONException;
 
 import java.io.IOException;
@@ -11,45 +15,54 @@ public class Main {
 
     public static void main(String[] args) {
         JiraJsonParser parser = new JiraJsonParser();
-        JiraToAzureTranslator translator = new JiraToAzureTranslator("ratiucosmina", "Robotic%20Arm%20CMMI", "bywbcvqjncljzxizgibtvatchkwggtok2qzztyetmugyco5zrhoq");
+        JiraToAzureTranslator translator = new JiraToAzureTranslator("ratiucosmina", "JiraTest4", "bywbcvqjncljzxizgibtvatchkwggtok2qzztyetmugyco5zrhoq");
+        try {
+            WorkItem item = translator.witApi.getWorkItem(6246, WorkItemExpand.ALL);
+            List<WorkItemRelations> relations = item.getRelations();
+            if(relations==null){
+                System.out.println("hellp");
+            }
+        } catch (AzDException e) {
+            throw new RuntimeException(e);
+        }
         try {
             List<Optional<Issue>> issues = parser.loadIssues();
             translator.translateArtifacts(issues);
-            Map<String, Integer> links = new HashMap<>();
-            for (Optional<Issue> issue : issues) {
-                Issue issue1 = issue.get();
-                for (IssueLink issueLink : issue1.getIssueLinks()) {
-                    String issueType = issue1.getIssueType().getName();
-                    String targetKey = issueLink.getTargetIssueKey();
-                    if(!translator.mapping.containsKey(targetKey)) {
-//                        System.out.println(targetKey+issueLink.getIssueLinkType().getName());
-//                        String link = " UNDEFINED TARGET: " + issueLink.getIssueLinkType().getName();
-//                        if (links.containsKey(link))
-//                            links.put(link, links.get(link) + 1);
-//                        else
-//                            links.put(link, 1);
-//                        String link2 = " UNDEFINED TARGET";
-//                        if (links.containsKey(link2))
-//                            links.put(link2, links.get(link2) + 1);
-//                        else
-//                            links.put(link2, 1);
-                        continue;
-                    }
-                    String targetType = translator.mapping.get(targetKey);
-                    if( issueLink.getIssueLinkType().getDirection().name().equals("INBOUND"))
-                        continue;
-                    String link = issueType + " -> " + issueLink.getIssueLinkType().getName() +" -> "+targetType;
-                    if (links.containsKey(link))
-                        links.put(link, links.get(link) + 1);
-                    else
-                        links.put(link, 1);
-                    //                    links.add(issueLink.getIssueLinkType().getName());
+//
+                    //      Map<String, Integer> links = new HashMap<>();
+            ////            for (Optional<Issue> issue : issues) {
+            ////                Issue issue1 = issue.get();
+            ////                for (IssueLink issueLink : issue1.getIssueLinks()) {
+            ////                    String issueType = issue1.getIssueType().getName();
+            ////                    String targetKey = issueLink.getTargetIssueKey();
+            ////                    if(!translator.mapping.containsKey(targetKey)) {
+            //////                        System.out.println(targetKey+issueLink.getIssueLinkType().getName());
+            //////                        String link = " UNDEFINED TARGET: " + issueLink.getIssueLinkType().getName();
+            //////                        if (links.containsKey(link))
+            //////                            links.put(link, links.get(link) + 1);
+            //////                        else
+            //////                            links.put(link, 1);
+            //////                        String link2 = " UNDEFINED TARGET";
+            //////                        if (links.containsKey(link2))
+            //////                            links.put(link2, links.get(link2) + 1);
+            //////                        else
+            //////                            links.put(link2, 1);
+            ////                        continue;
+            ////                    }
+            ////                    String targetType = translator.mapping.get(targetKey);
+            ////                    if( issueLink.getIssueLinkType().getDirection().name().equals("INBOUND"))
+            ////                        continue;
+            ////                    String link = issueType + " -> " + issueLink.getIssueLinkType().getName() +" -> "+targetType;
+            ////                    if (links.containsKey(link))
+            ////                        links.put(link, links.get(link) + 1);
+            ////                    else
+            ////                        links.put(link, 1);                links.add(issueLink.getIssueLinkType().getName());
 //                    links.add(issue1.getIssueType().getName()+" -> " +issueLink.getIssueLinkType().getName());
-                }
-            }
-            links.entrySet().stream().sorted(Comparator.comparingInt(l -> l.getValue())).forEach(l -> System.out.println(l.getKey() + "\t\t#"+l.getValue()));
+//                }
+//            }
+//            links.entrySet().stream().sorted(Comparator.comparingInt(l -> l.getValue())).forEach(l -> System.out.println(l.getKey() + "\t\t#"+l.getValue()));
         } catch (JSONException | IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
         }
     }
 }
